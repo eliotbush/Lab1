d94d67ce94edf3cb7d2e38a57713cba85daef42d
eliotbush <bush.eliot@gmail.com>
2015-10-07 16:29:58 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,77 @@
 //  Emma Bryce - Eliot Bush - Ygor Jean
-//  thermalShit.c
+//  thermalSimulation.c
 //  ECE 353 Lab 1
-//  9/25/15
-//test - push
+//  10/7/15
+////////////////////////////////////////////////////////////////////////////////////////
 
-//I don't think he wants us to call the file "thermalShit" but I didn't see a name specified in the assignment.
-//test
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
 #include <string.h>
 
+/////////////////////////////////////////////////////////////////////////////////////
+
+//globals we'll need
+double **thermalParam;
+double **powerTrace;
+int powerTraceLength;
+double ambientTemp;
+//equivalent resistance between the cores and ambient. this may need to be an array, need to ask Krishna about it
+double ambientR;
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 //Runge-Kutta Algorithm
-// 1. we need to define the differential of the effective age with respect to temperature before we can use this.
-// 2. we need to define the differential of the effective temperature with respect to time before we can use that...
-//defined h as step size, t as time and y as the ith order output...
-double rk(double(*f)(double, double), double h, double t, double y){
-    double	k1 = h * f(t, y),
-    k2 = h * f(t + h / 2, y + k1 / 2),
-    k3 = h * f(t + h / 2, y + k2 / 2),
-    k4 = h * f(t + h, y + k3);
-    return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+//This has been modified to solve a system of functions y1(t+h), y2(t+h), ... yn(t+h) given f(y,t)=y' as an input
+//n denotes how many functions are in the system
+
+double* rk(double* (*f)(double, double*), double h, double t, double *y, int n){
+	//k is a 4xn array storying (k10, k11,...k1n),(k20,...k2n)...(k40...k4n) 
+	double **k;
+	k = (double **) malloc(4*sizeof(double *));
+	int j;
+	for(j=0; j<4; j++){k[j] = (double *) malloc(n*sizeof(double));}
+	
+	//placeholder arrays for intermediate calculations
+	double *placeHolderY;
+	placeHolderY = (double *) malloc(n*sizeof(double));
+	double *placeHolderK;
+	placeHolderK = (double *) malloc(n*sizeof(double));
+
+	//placeholder for k1 calculation
+	placeHolderK = (*f)(t, y);
+	//do k1=h*f(t,y) term by term
+	for(j=0; j<n; j++){k[0][j] = h*placeHolderK[j];}
+
+	//do y+k1/2 term by term, store in placeholder
+	for(j=0; j<n; j++){placeHolderY[j] = y[j]+k[0][j]/2;}
+	//k2 placeholder
+	placeHolderK = (*f)((t+h/2), placeHolderY);
+	//k2=h*f(t+h/2,y+k1/2) term by term
+	for(j=0; j<n; j++){k[1][j] = h*placeHolderK[j];}
+
+	//same process as above for k3
+	for(j=0; j<n; j++){placeHolderY[j] = y[j]+k[1][j]/2;}
+	placeHolderK = (*f)((t+h/2), placeHolderY);
+	for(j=0; j<n; j++){k[2][j] = h*placeHolderK[j];}
+
+	//and k4
+	for(j=0; j<n; j++){placeHolderY[j] = y[j]+k[2][j];}
+	placeHolderK = (*f)((t+h), placeHolderY);
+	for(j=0; j<n; j++){k[3][j] = h*placeHolderK[j];}
+
+	//array to be returned
+	double *output;
+	output = (double *) malloc(n*sizeof(double));
+	//do yi(t+h) = yi(t) + (k1i + 2k2i + 2k3i + k4i)/6 term by term
+	for(j=0; j<n; j++){output[j] = y[j] + (k[0][j] + 2*k[1][j] + 2*k[2][j] + k[3][j])/6.0;}
+	return output;
 }
+
 ///////////////////////////////////////////////////////////////////////////////////
+
+//count the number of rows in the file
 int row_count(FILE *file){
        
         int count = 1;
@@ -43,6 +91,7 @@ int row_count(FILE *file){
 
 ///////////////////////////////////////////////////////////////////////////////////
 
+//take a file and turn it into an 2d array with the right number of rows and columns
 double** fileArray(FILE *file, int rows, int columns){
 	double **array;
 	int i;
@@ -73,25 +122,37 @@ double** fileArray(FILE *file, int rows, int columns){
 ///////////////////////////////////////////////////////////////////////////////
 
 //initialize the temperatures. just sets them all to ambient
-double* initializeT(double ambient){
+double* initializeT(){
 	double *temps;
 	temps = (double *) malloc(5*sizeof(double));
 	int i;
-	for(i=0; i<5; i++){temps[i] = ambient;}
+	for(i=0; i<5; i++){temps[i] = ambientTemp;}
 	return temps;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-//update the temperatures.
-//this should be using RK, although I don't really understand why it's necessary when you can just solve it explicitly.
-double* updateT(double *temps, double **param, double *trace, double h, double ambientR){
+//calculate dT/dt at T,t
+double* calculatedTdt(double t, double *temps){	
 	//dTdt[i] = dT_i/dt
 	double *dTdt;
-	dTdt = (double *) malloc(4*sizeof(double));
+	dTdt = (double *) malloc(5*sizeof(double));
 	int i;
 	//initialize dT/dt as 0 for all i
-	for(i=0; i<4; i++){dTdt[0]=0.0;}
+	for(i=0; i<5; i++){dTdt[0]=0.0;}
+
+	//work out which entry in the power trace we're currently on
+	//go through the power trace until the time entry in the trace exceeds the current time and save which step it's after
+	int step = 0;
+	i=0;
+	while (i<powerTraceLength){
+		if (powerTrace[i][0]>t){
+			step = i-1;
+			i = powerTraceLength;
+		}
+		else{i++;}
+	}
+
 	int j;
 	//iterate over each core
 	for(i=0; i<4; i++){
@@ -99,25 +160,24 @@ double* updateT(double *temps, double **param, double *trace, double h, double a
 		for(j=0; j<5; j++){
 			if(i!=j){
 				//dT_i/dt = -sum(j=0..3, i!=j) T_i-T_j/R_ijC_i
-				if(j!=4){dTdt[i] -= (temps[i]-temps[j])/(param[i][j]*param[0][i]);}
+				if(j!=4){dTdt[i] -= (temps[i]-temps[j])/(thermalParam[i][j]*thermalParam[0][i]);}
 				//and the ambient term T_i-T_amb/R_ambC_i
-				else{dTdt[i] -= (temps[i]-temps[j])/(ambientR*param[0][i]);}
+				else{dTdt[i] -= (temps[i]-temps[j])/(ambientR*thermalParam[0][i]);}
 			}
 		}
 		//and add the term from the dissipated power, omega_i/C_i
-		dTdt[i] += trace[i+1]/param[0][i];
+		dTdt[i] += powerTrace[step][i+1]/thermalParam[0][i];
 	}
-	//update the temperatures with dT/dt
-	for(i=0; i<4; i++){temps[i] += h * dTdt[i];}
-	return temps;
+
+	
+	return dTdt;
 }
 
 ///////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]){
 
-	double h= 0.05; //step size parameter to be passed into rk
-	double x0 = 0; //core 0
-	double x1 = 3; //core 3
+	double h = 0.005; //step size parameter to be passed into rk
+	double t = 0;
 
 	//thermal paramaters file
 	FILE *tpfp;
@@ -126,14 +186,21 @@ int main(int argc, char *argv[]){
 	//output file
 	FILE *ofp;
 
-	double ambientTemp=300;
+	//initialize ambient temperature and resistance
+	ambientTemp = 300;
+	ambientR = 1.0;
+
+	//check to see if an argument was passed for ambient temp
 	if(argc=5){
+	//if it was, set ambientTemp to it
 	sscanf(argv[3], "%lf", &ambientTemp);
+	//if there was, the output file is argument 4
 	ofp = fopen(argv[4], "w");}
 
 	//open the files
 	tpfp = fopen(argv[1], "rb");
 	ptfp = fopen(argv[2], "rb");
+	//if there wasn't an ambient temp argument, output file is argument 3
 	if(ofp==NULL){ofp = fopen(argv[3], "w");}
 
 	//make sure the files are there
@@ -141,55 +208,30 @@ int main(int argc, char *argv[]){
 	assert(ptfp != NULL);
 	assert(ofp != NULL);
 
-	//number of rows in each input file. initialized as 1 because otherwise it won't count the last row
-	int thermalParamLength= row_count(tpfp);
-	int powerTraceLength=row_count(ptfp);
+	//number of rows in each input file
+	int thermalParamLength=row_count(tpfp);
+	powerTraceLength=row_count(ptfp);
 
-	//declare pointer array thingies
-	//note that they're 2d. indexing is done by array[row][column]
-	//so for example if you want the power of node 2 at timestep 441, it's powerTrace[441][3]
-	double **thermalParam = fileArray(tpfp, thermalParamLength, 4);
-	double **powerTrace = fileArray(ptfp, powerTraceLength, 5);
+	//read the files into pointer arrays for thermal param and power trace
+	thermalParam = fileArray(tpfp, thermalParamLength, 4);
+	powerTrace = fileArray(ptfp, powerTraceLength, 5);
 
 	//T holds the temperatures. cores 0-3 are T[0]-T[3], T[4] is ambient
 	double *T;
 	T = initializeT(ambientTemp);
 
-	//print and test stuff for temperature function
-	printf("\nAmbient Temp:\n\n%lf\n\n", ambientTemp);
+	//step through updating the temperature
 	int i;
-	for(i=0; i<4; i++){
-		printf("T%i: %lf\n", i, T[i]); 
-	}
-	//step through updating the temperature for the length of powerTrace
-	int j;
-	for(j=0; j<powerTraceLength; j++){
+	int j=0;
+	double endTime = powerTrace[powerTraceLength-1][0];
+	while (t<=endTime){
 		printf("\n\nstep %i:\n", j);
-		T = updateT(T, thermalParam, powerTrace[j], h, 10.0);
+		printf("\ntime: %lf\n", t);
+		T = rk(&calculatedTdt, h, t, T, 5);
 		for(i=0; i<4; i++){
 			printf("T%i: %lf\n", i, T[i]); 
 		}
+		t+=h;
+		j++;
 	}
-
-/*
-	//more print stuff
-	printf("Thermal parameters:\n\n");
-	for(i=0; i<thermalParamLength; i++){
-		for(j=0; j<4; j++){
-			printf("%lf ", thermalParam[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\nPower Trace:\n\n");
-	for(i=0; i<powerTraceLength; i++){
-		for(j=0; j<5; j++){
-			printf("%lf ", powerTrace[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\nAmbient Temp:\n\n%lf\n\n", ambientTemp);
-	for(i=0; i<4; i++){
-		printf("T%i: %lf\n", i, T[i]); 
-	}
-*/
 }
